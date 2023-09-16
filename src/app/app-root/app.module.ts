b1ac7ef162061836ed4c758b0d273466417bf171
SabikAbtahee <sabikchamp@gmail.com>
2023-09-16 19:49:10 +0600

    chore: folder location formated

--- src/app/app-root/app.module.ts
+++ src/app/app-root/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from "../app-route/app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
 	declarations: [AppComponent],
