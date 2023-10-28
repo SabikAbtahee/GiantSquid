8d11f8c42ef762240f97aac10dd6d1bc3fb8cd31
SabikAbtahee <sabikchamp@gmail.com>
2023-10-29 01:18:10 +0600

    feat: avatar made

---
+++ src/app/app-common/avatar/avatar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+	selector: "app-avatar",
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: "./avatar.component.html",
+	styleUrls: ["./avatar.component.scss"]
+})
+export class AvatarComponent {
+	@Input() imageSrc = "assets/PortalLogo/Logo.svg";
+}
--- src/app/app-common/index.ts
+++ src/app/app-common/index.ts
@@ -1,3 +1,4 @@
 export * from "./language-picker/language-picker.component";
 export * from "./page-not-found/page-not-found.component";
 export * from "./full-screen/full-screen.component";
+export * from "./avatar/avatar.component";
