999468c86528a3a322b4a689aa7e4fd4bb9b9616
SabikAbtahee <sabikchamp@gmail.com>
2023-11-25 19:19:10 +0600

    chore: color related scss taken to single file

--- src/app/app-common/page-not-found/page-not-found.component.ts
+++ src/app/app-common/page-not-found/page-not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { MaterialModule } from "@material";
 
 @Component({
 	selector: "app-page-not-found",
 	standalone: true,
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, MaterialModule],
 	templateUrl: "./page-not-found.component.html",
 	styleUrls: ["./page-not-found.component.scss"]
 })
